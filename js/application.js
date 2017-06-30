066135619d9c4f4221f0e5ab77afe4f5ddbf7f15
Josh Lim <joshielim@gmail.com>
2017-06-29 17:11:29 -0700

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,33 +1,19 @@
 $(document).ready(function() {
-  var game = new Game();
-  game.display();
-
-  console.log(game.board);
-  Mousetrap.bind('left', function() {
-    game.move('left');
-    game.randomAdd();
-    console.log(game.board);
-    game.display();
+  var game = new Game
+  
+  Mousetrap.bind('left', function(){
+    game.moveLeft();
   })
-
-  Mousetrap.bind('right', function() {
-    game.move('right');
-    game.randomAdd();
-    console.log(game.board);
-    game.display();
+  
+  Mousetrap.bind('right', function(){
+    game.moveRight();
   })
-
-  Mousetrap.bind('up', function() {
-    game.move('up');
-    game.randomAdd();
-    console.log(game.board);
-    game.display();
+  
+  Mousetrap.bind('up', function(){
+    game.moveUp();
   })
-
-  Mousetrap.bind('down', function() {
-    game.move('down');
-    game.randomAdd();
-    console.log(game.board);
-    game.display();
+  
+  Mousetrap.bind('down', function(){
+    game.moveDown();
   })
 });
